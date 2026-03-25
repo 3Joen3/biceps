d6cbb95a849af0cd5571b6a30032476d3f3976d0
3Joen3 <joalevall@gmail.com>
2026-03-25 14:11:01 +0100

    Header changes

--- layout/Header.tsx
+++ layout/Header.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 export default function Header() {
   return (
     <header className="py-3">
-      <div className="w-[95%] mx-auto">
+      <div className="w-[94%] mx-auto">
         <Logo />
       </div>
     </header>
@@ -14,8 +14,8 @@ export default function Header() {
 function Logo() {
   return (
     <Link href="/" className="flex items-center gap-3">
-      <Image src="/images/biceps-logga.png" alt="Logotyp" height={70} width={70} />
-      <span className="uppercase font-bold text-6xl">Biceps</span>
+      <Image src="/images/biceps-logga.png" alt="Logotyp" height={64} width={64} />
+      <span className="uppercase font-extrabold text-5xl">Biceps</span>
     </Link>
   );
 }
