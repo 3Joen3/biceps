7f2e3a43a80b467f14632951d2d2862ad0c4a5c1
3Joen3 <joalevall@gmail.com>
2026-03-25 13:57:30 +0100

    Header for now

--- layout/Header.tsx
+++ layout/Header.tsx
@@ -1,3 +1,21 @@
+import Link from "next/link";
+import Image from "next/image";
+
 export default function Header() {
-  return <header className="bg-black text-white">Header</header>;
+  return (
+    <header className="py-3">
+      <div className="w-[95%] mx-auto">
+        <Logo />
+      </div>
+    </header>
+  );
+}
+
+function Logo() {
+  return (
+    <Link href="/" className="flex items-center gap-3">
+      <Image src="/images/biceps-logga.png" alt="Logotyp" height={70} width={70} />
+      <span className="uppercase font-bold text-6xl">Biceps</span>
+    </Link>
+  );
 }
