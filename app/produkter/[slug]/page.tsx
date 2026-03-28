596e7361c5cbd37583bd6423fb911cbeac6986ba
3Joen3 <joalevall@gmail.com>
2026-03-28 10:30:24 +0100

    Can commit this for now

---
+++ app/produkter/[slug]/page.tsx
@@ -0,0 +1,26 @@
+import { getProductBySlug } from "@/products/service";
+
+import Page from "@/layout/Page";
+import Image from "next/image";
+import BuySection from "@/products/components/BuySection";
+
+interface Props {
+  params: { slug: string };
+}
+
+export default async function ProductPage({ params }: Props) {
+  const { slug } = await params;
+  const product = await getProductBySlug(slug);
+
+  return (
+    <Page className="grid grid-cols-2 gap-3">
+      <div className="aspect-square relative border">
+        <Image src="/images/dummy/hantlar-5-kg.png" alt="Dummy Image" fill />
+      </div>
+      <div className="flex flex-col gap-3">
+        <h1 className="text-3xl font-semibold">{product.name}</h1>
+        <BuySection />
+      </div>
+    </Page>
+  );
+}
---
+++ products/components/BuySection.tsx
@@ -0,0 +1,3 @@
+export default function BuySection() {
+  return <div>BuySection</div>;
+}
