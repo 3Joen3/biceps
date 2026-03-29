f0050a29e9db57ced0b4ccc4a214a3f4df9620de
3Joen3 <joalevall@gmail.com>
2026-03-29 13:13:25 +0200

    Make the product page component some clearer while working

--- app/produkter/[slug]/page.tsx
+++ app/produkter/[slug]/page.tsx
@@ -14,13 +14,19 @@ export default async function ProductPage({ params }: Props) {
 
   return (
     <Page className="grid grid-cols-2 gap-3">
-      <div className="aspect-square relative border rounded-xl overflow-hidden">
-        <Image src="/images/dummy/hantlar-5-kg.png" alt="Dummy Image" fill />
-      </div>
+      <Dummy />
       <div className="flex flex-col gap-3">
         <h1 className="text-3xl font-semibold uppercase">{product.name}</h1>
         <BuySection product={product} />
       </div>
     </Page>
+  );
+}
+
+function Dummy() {
+  return (
+    <div className="aspect-square relative border rounded-xl overflow-hidden">
+      <Image src="/images/dummy/hantlar-5-kg.png" alt="Dummy Image" fill />
+    </div>
   );
 }
